e24b3999e569113248a789a01314df53a1865ac8
Amrit Ayalur <a.ayalur@gmail.com>
2013-12-13 02:15:36 -0800

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shawarma::Application.config.secret_token = 'cdd10b5f0ecf729b1bcadb41f47214839d47699ab438b7c6fb82d8cec953aa76af564e0495426ed1a425d87e59847ae6fbeae39443e19522bb69ee5b96d6f486'
